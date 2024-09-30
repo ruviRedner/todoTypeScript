b66d944a01c99822b0fa4f577e13d1f10ba335c2
ruviRedner <r.y.r7966@gmail.com>
2024-09-30 10:18:57 +0300

    user model added

---
+++ src/types/models/user.ts
@@ -0,0 +1,20 @@
+import { v4 } from "uuid"
+import bcrypt from "bcrypt"
+
+export default class User{
+    public id:string
+    public password?:string
+
+    constructor(
+        public username:string,
+
+    ){
+        this.id = v4();
+    }
+        async hashPassword(_password:string):Promise<void>{
+        this.password = await bcrypt.hash(_password,10)
+    }
+    async comparePassword(_password:string):Promise<boolean>{
+        return await bcrypt.compare(_password,this.password || "")
+    }
+}
